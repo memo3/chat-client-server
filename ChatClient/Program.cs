02ec48c2d6bc590e1013edaac680b850219d08a2
Gae <g.memoli11@gmail.com>
2021-05-01 23:11:23 +0200

    Aggiungere i file di progetto.

---
+++ ChatClient/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace ChatClient
+{
+    class Program
+    {
+
+        static string _guestName = "";
+
+        public static string GuestName { get => _guestName; set => _guestName = value; }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Chat Application";
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 10000;
+            TcpClient client = new TcpClient();
+
+            client.Connect(ip, port);
+
+            NetworkStream ns = client.GetStream();
+
+            // Initialize guest name
+            GuestName = getGuestName(ns);
+
+
+
+            // Starts a new thread for each client
+            Thread thread = new Thread(o => ReceiveData((TcpClient)o));
+            thread.Start(client);
+
+            // Welcome the user
+            Console.WriteLine($"Welcome {GuestName}!");
+
+            string input;
+
+            // Listens to keyboard input
+            while (!string.IsNullOrEmpty((input = Console.ReadLine())))
+            {
+                    ClearCurrentConsoleLine();
+                    writeToStream($"CHAT:{GuestName}>{input}");
+            }
+
+            // Notifies the users when someone leave the room
+            writeToStream($"EXIT:{GuestName}");
+
+
+            client.Client.Shutdown(SocketShutdown.Send);
+            thread.Join();
+            ns.Close();
+            client.Close();
+            Console.ReadKey();
+
+
+            // Helper function: broadcast when the user left the room
+            void writeToStream(string data)
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                ns.Write(buffer, 0, buffer.Length);
+            }
+
+        }
+
+        // Invoked when data is sent to the stream, it serves as an active connection between client(s) and the server
+        static void ReceiveData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] receivedBytes = new byte[1024];
+            int byte_count;
+
+            while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+            {
+
+                // Converts the bytestream to string
+                var data = Encoding.ASCII.GetString(receivedBytes, 0, byte_count);
+
+                // Splits the message type and the actual message
+                var parsedData = data.Split(':');
+                string code = parsedData[0];
+                string payload = parsedData[1];
+
+                // Removed unwanted carriage return and new line
+                payload = payload.TrimEnd('\r', '\n');
+                if (code == "EXIT")
+                {
+                    if (GuestName != payload)
+                    {
+                        Console.WriteLine($"\n({payload} has left the room.)\n");
+                    } else
+                    {
+                        Console.WriteLine($"\n(You left the room.)\nPress 'enter' to quit.");
+                    }
+                }
+
+            }
+
+        }
+
+        // Generate a temporary guest name based on the current number of users
+        static string getGuestName(NetworkStream ns)
+        {
+            byte[] receivedBytes = new byte[1024];
+            int byte_count;
+
+            byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length);
+
+            // Converts the bytestream to string
+            var data = Encoding.ASCII.GetString(receivedBytes, 0, byte_count);
+
+            Console.WriteLine(data);
+
+            // Splits the message type and the actual message
+            var parsedData = data.Split(':');
+
+           
+
+            string code = parsedData[0];
+            string payload = parsedData[1].TrimEnd('\r', '\n'); ;
+
+            return $"{payload}";
+        }
+
+
+        public static void ClearCurrentConsoleLine()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+
+
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+
+namespace ChatServer
+{
+    class Program
+    {
+        static readonly object _lock = new object();
+        static readonly Dictionary<int, TcpClient> list_clients = new Dictionary<int, TcpClient>();
+
+        static List<string> userList = new List<string>();
+
+
+        static void Main(string[] args)
+        {
+            int count = 1;
+
+            TcpListener ServerSocket = new TcpListener(IPAddress.Any, 10000);
+            ServerSocket.Start();
+
+            Console.WriteLine($"Server started: {IPAddress.Any}:10000");
+
+            while (true)
+            {
+                TcpClient client = ServerSocket.AcceptTcpClient();
+                lock (_lock) list_clients.Add(count, client);
+
+
+                // When the client is connected, broadcast the current number of user(s)
+                //broadcast($"NUMBER:{list_clients.Count}");
+
+                broadcast($"NEW_GUEST:GUEST{list_clients.Count.ToString("D3")}");
+                Console.WriteLine($"GUEST{list_clients.Count} has joined the room");
+                
+
+                Thread t = new Thread(handle_clients);
+                t.Start(count);
+                count++;
+            }
+        }
+
+        public static int getUserCount()
+        {
+            return list_clients.Count;
+        }
+
+        public static void handle_clients(object o)
+        {
+
+
+            int id = (int)o;
+            TcpClient client;
+
+            lock (_lock) client = list_clients[id];
+
+            while (true)
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int byte_count = stream.Read(buffer, 0, buffer.Length);
+
+                if (byte_count == 0)
+                {
+                    break;
+                }
+                
+                // Converts the bytestream to string
+                var data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+                broadcast(data);
+
+                // Splits the message type and the actual message
+                var parsedPayload = data.Split(':');
+
+                //Console.WriteLine(payload);
+
+                string code = parsedPayload[0];
+                string payload = parsedPayload[1];
+                
+            }
+
+            lock (_lock) list_clients.Remove(id);
+            client.Client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
+        public static void broadcast(string data)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(data + Environment.NewLine);
+
+            lock (_lock)
+            {
+                foreach (TcpClient c in list_clients.Values)
+                {
+                    NetworkStream stream = c.GetStream();
+
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
